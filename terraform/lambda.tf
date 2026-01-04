3dd225f38c542d24e61cad764dba6a3f2f279cc9
Noriki Nakamura <noriki.nakamura.0424@gmail.com>
2026-01-04 22:39:02 +0900

    Use Node.js 22.x

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "email_sender" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs20.x"
+  runtime          = "nodejs22.x"
 
   environment {
     variables = {
